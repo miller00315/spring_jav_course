c0a8a028f19a0bd5100b188a58efcc24954f0529
Miller Oliveira <mcesar@ocatec.com.br>
2020-06-17 14:51:33 -0300

    cliando crud de client

--- src/main/java/com/miller/osWorks/api/controller/ClientController.java
+++ src/main/java/com/miller/osWorks/api/controller/ClientController.java
@@ -1,36 +1,81 @@
 package com.miller.osWorks.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.miller.osWorks.domain.model.Client;
+import com.miller.osWorks.domain.repository.ClientRepository;
 
 /**
  * @author miller
  */
 
 @RestController
+@RequestMapping("/clients")
 public class ClientController {
 	
+	
+	@Autowired
+	private ClientRepository clientRepository;
+	
 	// Returns all the clients
-	@GetMapping("/clients")
+	@GetMapping()
 	public List<Client> list() {
-		Client client1 = new Client();
-		Client client2 = new Client();
+		return clientRepository.findAll();
+	}
+	
+	// Return a client by id
+	@GetMapping("/{clientId}")
+	public ResponseEntity<Client> search(@PathVariable Long clientId) {	
+		Optional<Client> client =  clientRepository.findById(clientId);
 		
-		client1.setId(1L);
-		client1.setName("Miller Oliveira");
-		client1.setEmail("miller00315@gmail.com");
-		client1.setPhone("23456789");
+		if(client.isPresent()) {
+			return ResponseEntity.ok(client.get());
+		}
 		
-		client2.setId(2L);
-		client2.setName("Carlos");
-		client2.setEmail("carlos@gmail.com");
-		client2.setPhone("90876543");
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Client add(@RequestBody Client client) {
+		return clientRepository.save(client);
+	}
+	
+	@PutMapping("/{clientId}")
+	public ResponseEntity<Client> update(@PathVariable Long clientId, @RequestBody Client client) {
 		
-		return Arrays.asList(client1, client2);
+		if(!clientRepository.existsById(clientId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		client.setId(clientId);
+		client = clientRepository.save(client);
+		
+		return ResponseEntity.ok(client);
+	}
+	
+	@DeleteMapping("/{clientId}")
+	public ResponseEntity<Void> delete(@PathVariable Long clientId) {
+		if(!clientRepository.existsById(clientId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		clientRepository.deleteById(clientId);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
