05736694957802139e2c423973370d87b7737110
Miller Oliveira <mcesar@ocatec.com.br>
2020-06-16 23:05:58 -0300

    Iniciando criação de entidades

--- src/main/java/com/miller/osWorks/api/controller/ClientController.java
+++ src/main/java/com/miller/osWorks/api/controller/ClientController.java
@@ -1,7 +1,12 @@
 package com.miller.osWorks.api.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.miller.osWorks.domain.model.Client;
 
 /**
  * @author miller
@@ -10,13 +15,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ClientController {
 	
-	/**
-	 * 
-	 * @return String
-	 */
-	
+	// Returns all the clients
 	@GetMapping("/clients")
-	public String list() {
-		return "test";
+	public List<Client> list() {
+		Client client1 = new Client();
+		Client client2 = new Client();
+		
+		client1.setId(1L);
+		client1.setName("Miller Oliveira");
+		client1.setEmail("miller00315@gmail.com");
+		client1.setPhone("23456789");
+		
+		client2.setId(2L);
+		client2.setName("Carlos");
+		client2.setEmail("carlos@gmail.com");
+		client2.setPhone("90876543");
+		
+		return Arrays.asList(client1, client2);
 	}
 }
